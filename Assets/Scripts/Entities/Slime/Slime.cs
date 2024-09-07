06ea619ea5440d190d70a1e4f5b88d59d48add49
0xVector <hello@0xvector.me>
2024-09-07 18:04:14 +0200

    Remove client-side slime rotation

--- Assets/Scripts/Entities/Slime/Slime.cs
+++ Assets/Scripts/Entities/Slime/Slime.cs
@@ -2,12 +2,4 @@ using UnityEngine;
 
 public class Slime : Entity
 {
-    protected override void Update() {
-        // Client-side rotation
-        float x = rb.velocity.x;
-        if (x > 0) dir = Direction.Right;
-        else if (x < 0) dir = Direction.Left;
-
-        base.Update();
-    }
 }
